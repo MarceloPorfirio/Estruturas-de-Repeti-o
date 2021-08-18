cf652f83c9e7b7e43ebcd92c63540b7b5fcafe21
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2021-08-18 20:45:21 -0300

    Add files via upload

---
+++ AlunosWhile.js
@@ -0,0 +1,8 @@
+let alunos = ['Marcelo','Leonardo','Gabriela','José','julia']
+let contador = 0
+
+while (contador < alunos.length){
+    let nome_aluno = alunos [contador]
+    console.log('Aluno: ' + nome_aluno )
+    contador++
+}
